a7d5b89829b4159412f901078f2d509a5762c032
Fatimah <Fatimahse25@gmail.com>
2023-12-18 10:25:58 +0500

    Initial commit: Added implementation of DIsitinct.

---
+++ Sorting/Distinct.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+namespace CodilityTasks
+{
+	class Distinct
+
+	{
+	public:
+		int solution(std::vector<int> &A);
+	};
+}
+
+int CodilityTasks::Distinct::solution(std::vector<int> &A)
+{
+	if (A.empty())
+	{
+		return 0;
+	}
+
+	std::sort(A.begin(), A.end());
+	int result = 1; // At least one distinct element (the first one)
+	for (int i = 1; i < A.size(); ++i)
+	{
+		if (A[i] != A[i - 1])
+		{
+			result++;
+		}
+	}
+	return result;
+}
+
+int main()
+{
+	CodilityTasks::Distinct Distinct;
+	std::vector<int> A = {1, 2, 3, 2, 1, 1};
+	int result = Distinct.solution(A);
+	std::cout << "Disitinct Number in vector is: " << result << std::endl;
+	return 0;
+}
