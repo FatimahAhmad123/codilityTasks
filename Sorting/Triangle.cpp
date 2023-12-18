f03deb7dabeb1269e73038f2cb441756f8b0d361
Fatimah <Fatimahse25@gmail.com>
2023-12-18 13:03:51 +0500

    Added Implementation of Triangle

---
+++ Sorting/Triangle.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+namespace CodilityTasks
+{
+	class Triangle
+
+	{
+	public:
+		int solution(std::vector<int> &A);
+	};
+}
+
+int CodilityTasks::Triangle::solution(std::vector<int> &A)
+{
+	if (A.size() < 3)
+	{
+		return 0;
+	}
+
+	std::sort(A.begin(), A.end());
+
+	for (std::size_t i = 0; i < A.size() - 2; ++i)
+	{
+		long long sum = static_cast<long long>(A[i]) + A[i + 1];
+
+		if (sum > A[i + 2])
+		{
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int main()
+{
+	CodilityTasks::Triangle Triangle;
+	std::vector<int> A = {10, 2, 5, 1, 8, 20};
+	int result = Triangle.solution(A);
+	std::cout << "The Result is: " << result << std::endl;
+	return 0;
+}
