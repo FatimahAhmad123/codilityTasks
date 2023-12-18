61665fc7d77f08353e7dd6c42274947e5890d41b
Fatimah <Fatimahse25@gmail.com>
2023-12-18 10:05:23 +0500

    Added implementation of CountDiv

---
+++ PrefixSums/CountDiv.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+namespace CodilityTasks
+{
+	class CountDiv
+
+	{
+	public:
+		int solution(int A, int B, int K);
+	};
+}
+
+int CodilityTasks::CountDiv::solution(int A, int B, int K)
+{
+	int inclusive = 0;
+	if (A % K == 0)
+	{
+		inclusive = 1;
+	}
+	return (B / K) - (A / K) + inclusive;
+}
+
+int main()
+{
+	CodilityTasks::CountDiv CountDiv;
+	int A = 6;
+	int B = 11;
+	int K = 2;
+	int result = CountDiv.solution(A, B, K);
+	std::cout << "Number of integers divisible by " << K << " in the range [" << A << "-" << B << "]: " << result << std::endl;
+
+	return 0;
+}
