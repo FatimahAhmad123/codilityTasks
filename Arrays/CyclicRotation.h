ec0ff247e454caa528d372fe30688563cc59c4ab
Fatimah <Fatimahse25@gmail.com>
2023-12-18 13:16:59 +0500

    Improvement: Added header files for both Tasks

---
+++ Arrays/CyclicRotation.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+namespace CodilityTasks
+{
+	class CyclicRotation
+	{
+	public:
+		static std::vector<int> solution(std::vector<int> &A, int k);
+	};
+}
---
+++ Arrays/OddOccurencesInArray.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+namespace CodilityTasks
+{
+	class OddOccurencesInArray
+	{
+	public:
+		int solution(std::vector<int> &A);
+	};
+}
