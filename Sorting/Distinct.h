fc4c44da5ae81c524d3f11d374cae2965fab3a47
Fatimah <Fatimahse25@gmail.com>
2023-12-19 11:50:20 +0500

    Improvement: Added Header files for each task.

---
+++ Sorting/Distinct.h
@@ -0,0 +1,16 @@
+#ifndef DISTINCT.H
+#define DISTINCT .H
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+namespace CodilityTasks
+{
+	class Distinct
+
+	{
+	public:
+		int solution(std::vector<int> &A);
+	};
+}
+#endif DISTINCT.H
