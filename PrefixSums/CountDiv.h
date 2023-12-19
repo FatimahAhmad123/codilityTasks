3b12d6765b7109bdfe3b32f6212fdd46b38deef2
Fatimah <Fatimahse25@gmail.com>
2023-12-19 11:42:58 +0500

    Improvement: Added Header files for each task.

---
+++ PrefixSums/CountDiv.h
@@ -0,0 +1,15 @@
+#ifndef COUNTDIV.H
+#define COUNTDIV .H
+#include <iostream>
+#include <vector>
+
+namespace CodilityTasks
+{
+	class CountDiv
+
+	{
+	public:
+		int solution(int A, int B, int K);
+	};
+}
+#endif COUNTDIV.H
---
+++ PrefixSums/PassingCars.h
@@ -0,0 +1,15 @@
+#ifndef COUNTDIV.H
+#define COUNTDIV .H
+#include <iostream>
+#include <vector>
+
+namespace CodilityTasks
+{
+	class PrefixSum
+
+	{
+	public:
+		int solution(std::vector<int> &A);
+	};
+}
+#endif COUNTDIV.H
