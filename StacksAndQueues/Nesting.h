edea427e4ba31090838636359094460cef51f60a
Fatimah <Fatimahse25@gmail.com>
2023-12-20 10:03:54 +0500

    Added implementation of Nesting and added header file as well.

---
+++ StacksAndQueues/Nesting.h
@@ -0,0 +1,15 @@
+#ifndef NESTING_H
+#define NESTING_H
+#include <iostream>
+#include <string>
+#include <stack>
+
+namespace CodilityTasks
+{
+	class Nesting
+	{
+	public:
+		int solution(std::string &S);
+	};
+}
+#endif
